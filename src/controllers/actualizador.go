f44e923e4208f033a95ee287deb605efed015ec9
julianbecerra13 <becerrarodriguezjulian@gmail.com>
2023-05-05 17:15:38 -0500

    arreglo de el call de actualizar

--- src/controllers/actualizador.go
+++ src/controllers/actualizador.go
@@ -11,23 +11,19 @@ type ActualizadorRequest struct {
 	IdPedido       string `json:"idPedido"`
 	IdUsuario      string `json:"idUsuario"`
 	IdTraza        string `json:"idTraza"`
-	IdAdicional    string `json:"idAdicional"`
-	Campo          string `json:"campo"`
 	IdDomiciliario string `json:"idDomiciliario"`
 }
 
 func Actualizar(c *gin.Context) {
 	var actualizadorRequest ActualizadorRequest
 
-	actualizadorRequest.IdPunto = c.Query("idPunto")
-	actualizadorRequest.IdPedido = c.Query("idPedido")
-	actualizadorRequest.IdUsuario = c.Query("idUsuario")
-	actualizadorRequest.IdAdicional = c.Query("idAdicional")
-	actualizadorRequest.IdTraza = c.Query("idTraza")
-	actualizadorRequest.Campo = c.Query("campo")
-	actualizadorRequest.IdDomiciliario = c.Query("idDomiciliario")
+	if err := c.ShouldBindJSON(&actualizadorRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-	result := db.Raw("CALL spcp_sil_creatraza_motorizado(?, ?, ?, ?, ?, ?, ?)", actualizadorRequest.IdPunto, actualizadorRequest.IdPedido, actualizadorRequest.IdUsuario, actualizadorRequest.IdAdicional, actualizadorRequest.IdTraza, actualizadorRequest.Campo, actualizadorRequest.IdDomiciliario)
+	result := db.Exec("CALL spcp_sil_creatraza_motorizado(?, ?, ?, ?, ?, ?, ?)", actualizadorRequest.IdPunto, actualizadorRequest.IdPedido, actualizadorRequest.IdUsuario, "1", actualizadorRequest.IdTraza, "", actualizadorRequest.IdDomiciliario)
+
 	if result.Error != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": result.Error.Error()})
 		return
