33c17cb437523b8173aa04754d99c636644d95c2
perritwiguer130623 <becerrarodriguezjulian@gmail.com>
2023-06-23 11:10:42 -0500

    finalizado

--- src/controllers/cuadrecajapunto.go
+++ src/controllers/cuadrecajapunto.go
@@ -8,21 +8,22 @@ import (
 )
 
 type CuadreCajaPuntoRequest struct {
-	IDusuario string `json:"idDomiciliario"`
-	IDpunto        string `json:"idPunto"`
-	FechaInicio    string `json:"fechaInicio"`
-	FechaFin       string `json:"fechaFin"`
+	IDusuario   string `json:"idUsuario"`
+	IDpunto     string `json:"idPunto"`
+	FechaInicio string `json:"fechaInicio"`
+	FechaFin    string `json:"fechaFin"`
 }
 
 func CuadreCajaPunto(c *gin.Context) {
 
 	var cuadreCajaPuntoRequest CuadreCajaPuntoRequest
 	var cuadreCajaPunto []models.CuadreCajaPunto
+	
+	cuadreCajaPuntoRequest.IDusuario = c.Query("idUsuario")
+	cuadreCajaPuntoRequest.IDpunto = c.Query("idPunto")
+	cuadreCajaPuntoRequest.FechaInicio = c.Query("fechaInicio")
+	cuadreCajaPuntoRequest.FechaFin = c.Query("fechaFin")
 
-	if err := c.ShouldBindJSON(&cuadreCajaPuntoRequest); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
 
 	result := db.Raw("CALL spcp_sil_cuadrecajapunto(?,?,?,?)", cuadreCajaPuntoRequest.IDusuario, cuadreCajaPuntoRequest.IDpunto, cuadreCajaPuntoRequest.FechaInicio, cuadreCajaPuntoRequest.FechaFin).Scan(&cuadreCajaPunto)
 	if result.Error != nil {
@@ -31,4 +32,5 @@ func CuadreCajaPunto(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"cuadreCaja": cuadreCajaPunto})
+
 }
