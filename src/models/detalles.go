48b88ffd1b89ad4faca7a2f2e35f4c8a2da0c66f
perritwiguer130623 <becerrarodriguezjulian@gmail.com>
2023-07-17 22:13:39 -0500

    arreglos nuevos

--- src/models/detalles.go
+++ src/models/detalles.go
@@ -1,8 +1,8 @@
 package models
 
 type Detalles struct {
+	Cantidad int `gorm:"column:cantidad"`
 	ItemNombre string `gorm:"column:itemNombre"`
 	ValorBaseUni string `gorm:"column:valorBaseUni"`
 	ValorTotal int `gorm:"column:valorTotal"`
-
 }
--- src/models/pedidos.go
+++ src/models/pedidos.go
@@ -7,4 +7,7 @@ type Pedidos struct {
 	TotalOrden     float64 `gorm:"column:totalOrden"`
 	FechaCrea      string  `gorm:"column:fechaCrea"`
 	NombreTraza    string  `gorm:"column:nombreTraza"`
+	PuntodeVenta   string  `gorm:"column:PuntoVenta"`
+	Observaciones  string  `gorm:"column:observaciones"`
+	Telefono       string  `gorm:"column:telefono"`
 }
